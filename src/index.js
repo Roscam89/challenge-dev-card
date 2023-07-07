7e209f1f8e51dc1b5fd3d87f5f3da33332a8f01d
Roscam89 <83566347+Roscam89@users.noreply.github.com>
2023-07-07 16:46:44 -0700

    modified:   public/index.html 	new file:   public/passport photo (3).jpg 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skills = [
+  {
+    skill: "html+CSS",
+    level: "beginner",
+    color: "#2662EA",
+  },
+
+  {
+    skill: "Javascript",
+    level: "intermediate",
+    color: "#EFDB810",
+  },
+
+  {
+    skill: "AndroidStudio+Java",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+
+  {
+    skill: "NodeJS + MongoDB",
+    level: "beginner",
+    color: "#E84F33",
+  },
+
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+
+  {
+    skill: "GitHub",
+    level: "intermediate",
+    color: "#EFD23F",
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Avatar />
+      <Intro />
+      <SkillList />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="passport photo (3).jpg" alt="dev avatar" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1 className="dev-name">Maxim Rosca</h1>
+      <p className="about">
+        Beginer front-end software developer,learning new skills on Udemy.com .
+        I like to play computer games,hang out with friends and travel. Also I
+        enjoy studying programming.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skills">
+      {skills.map((skillEl) => (
+        <Skill skillObj={skillEl} key={skillEl.skill} />
+      ))}
+    </ul>
+  );
+}
+
+function Skill({ skillObj }) {
+  const className = skillObj.skill;
+  const cssClassName = className.slice(0, 4).toLowerCase();
+  return (
+    <li
+      className={`${cssClassName}`}
+      style={{ backgroundColor: skillObj.color }}
+    >
+      {skillObj.skill}
+      {skillObj.level === "beginner"
+        ? "😊"
+        : skillObj.level === "intermediate"
+        ? "👍🏻"
+        : "💪🏻"}
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
